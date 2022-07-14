2b77983e2b680fe22693723cbab78dea20847077
tim <tim@timdeMacBook-Pro.local>
2022-07-15 01:00:29 +0800

    l6 memory management

---
+++ l6_Virtual_Memory_Management/src/timer.c
@@ -0,0 +1,22 @@
+#include "utils.h"
+#include "printf.h"
+#include "sched.h"
+#include "peripherals/timer.h"
+
+const unsigned int interval = 200000;
+unsigned int curVal = 0;
+
+void timer_init(void)
+{
+    curVal = get32(TIMER_CLO);
+    curVal += interval;
+    put32(TIMER_C1, curVal); // set first time of interrupt
+}
+
+void handle_timer_irq(void)
+{
+    curVal += interval;
+    put32(TIMER_C1, curVal); // update for next interrupt
+    put32(TIMER_CS, TIMER_CS_M1);
+    timer_tick();
+}
