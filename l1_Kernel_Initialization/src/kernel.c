7360b219c6c1c8071b31dc4c2daf013c780b4ce8
TIM <shinting840@gmail.com>
2022-11-08 23:26:39 +0800

    Try to use all 4 processor cores
    print "Hello, from processor <processor index>" for all of the cores.
    Set up a separate stack for each core and make sure that UART is
    initialized only once.

--- l1_Kernel_Initialization/src/kernel.c
+++ l1_Kernel_Initialization/src/kernel.c
@@ -1,11 +1,38 @@
 #include "pl011_uart.h"
+#include "utils.h"
 
-void kernel_main(void)
+static unsigned int semaphore = 0;
+
+void kernel_main(unsigned int core_id)
 {
-    uart_init();
-    uart_send_string("Hello, Paspberry 3B+\r\n");
+    const unsigned int core_id_from_func = proc_id();
+    
+    // Do the UART initialization only for CORE #0
+    if (core_id_from_func == 0) {
+        uart_init();
+    }
 
-    while (1) {
-        uart_send(uart_recv());
+    // Wait for previous CORE to finish displaying
+    while (core_id_from_func != semaphore) {}
+
+    uart_send_string("Hello, from processor core_id ");
+    uart_send(core_id + '0');
+    uart_send_string("\r\n");
+
+    uart_send_string("Hello, from processor core_id_from_func ");
+    uart_send(core_id_from_func + '0');
+    uart_send_string("\r\n");
+
+    // Counts for next CORE to go
+    ++semaphore;
+
+    // Do the echo only for CORE #0
+    if (core_id_from_func == 0) {
+        // Wait for all cores to finish
+        while (semaphore != 4) { }
+
+        while (1) {
+            uart_send(uart_recv());
+        }
     }
 }
